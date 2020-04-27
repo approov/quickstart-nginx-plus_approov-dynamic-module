03c51d6b588d8d1c4ca2573e004110ef99af41ec
Exadra37 <exadra37@gmail.com>
2020-04-27 11:14:18 +0100

    Improved safety checks for the Approov Token Binding.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- etc/nginx/modules/approov/token-binding-check.lua
+++ etc/nginx/modules/approov/token-binding-check.lua
@@ -5,14 +5,15 @@ local Approov = {}
 
 function Approov.checkTokenBinding(pay_claim, token_binding)
 
-    if pay_claim == nil then
-        ngx.log(ngx.WARN, "Approov Token Binding not present.")
-        return
-    end
-
-    if token_binding == nil then
+    if token_binding == nil or token_binding == "" then
         ngx.log(ngx.ERR, "Missing the value for the token binding header.")
         ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    end
+
+    -- This check must be always performed after checking `token_binding == nil`.
+    if pay_claim == nil or pay_claim == "" then
+        ngx.log(ngx.WARN, "Approov Token Binding not present.")
+        return
     end
 
     local sha256 = resty_sha256:new()
